22aa7f9c0ce6d54315aa7da4eb34654aef6960ed
Elvis Popović <elvis_popovic@yahoo.com>
2018-11-11 12:53:42 +0100

    Squat click response added
    Squat has its handler. Temporaly Debug message only.

---
+++ Assets/Scripts/Framework/Buttons/IButtonHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Buttons
+{
+    public interface IButtonHandler
+    {
+        void Squat_Click();
+    }
+
+}
---
+++ Assets/Scripts/Framework/Buttons/SquatHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Buttons
+{
+
+    public class SquatHandler : MonoBehaviour, IButtonHandler
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Squat_Click()
+        {
+            Debug.Log("Squat click: clicked");
+        }
+    }
+
+
+}
