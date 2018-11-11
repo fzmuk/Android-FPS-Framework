0b13b61a7d8446fc11f088ffd4d99eb2693637fd
Ariana-Lea Zuber <azuber@foi.hr>
2018-11-11 21:36:02 +0100

    Joystick and player movement

---
+++ Assets/Scripts/Framework/PlayerMovement/MovePlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using DisplacementDetection;
+
+namespace PlayerMovement
+{
+    public class MovePlayer
+    {
+
+        private GameObject player;
+        private IDisplacement displacementInput;  
+
+        public MovePlayer (GameObject player, IDisplacement displacementInput)
+        {
+            this.player = player;
+            this.displacementInput = displacementInput;
+        }
+
+        public void PlayerMove(int slow)
+        {
+            var inputVector = displacementInput.Shift;
+            var agent = player.GetComponent<NavMeshAgent>();
+            Vector3 moveVector;
+            if (inputVector.x != 0 && inputVector.y != 0)
+            {
+                moveVector = new Vector3(inputVector.x, 0, inputVector.y);
+                agent.Move(moveVector*(agent.speed/ slow));
+            }
+                 
+        }
+
+    }
+}
---
+++ Assets/Scripts/Game/Joystick.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DisplacementDetection;
+
+public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler {
+
+    public VirtualJoystick virtualJoystick;
+
+    private Image joystickButton;
+    private Image joystick;
+
+    private void Start()
+    {
+        joystick = GetComponent<Image>();
+        joystickButton = transform.GetChild(0).GetComponent<Image>();
+
+        virtualJoystick = new VirtualJoystick(joystick, joystickButton);
+        
+    }
+
+    public void OnDrag(PointerEventData ped)
+    {
+        if (virtualJoystick.JoystickHit(ped))
+        {
+            virtualJoystick.MoveJoistickButton();
+        }
+    }
+
+    public void OnPointerDown(PointerEventData ped)
+    {
+        virtualJoystick.PressedJoystick();
+        OnDrag(ped);
+    }
+
+    public void OnPointerUp(PointerEventData ped)
+    {
+        virtualJoystick.ReleaseJoystick();
+    }
+
+   
+}
+
