259485df2a8b8f5dbdd34a449ac2d8cf2b0828ca
Elvis Popović <elvis_popovic@yahoo.com>
2018-12-02 22:56:15 +0100

    Test commit
    Experimental folders with examples that separates Framework and Game modules

---
+++ Assets/Scripts/Framework/Experimental/BallisticFramework.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Framework.Experimental
+{
+    public class BallisticFramework : IBallisticFramework
+    {
+        public void Init()
+        {
+            Debug.Log("Framework: Ballistic initialized.");
+        }
+
+        public void OnUpdate()
+        {
+            Debug.Log("Framework: Ballistic updated.");
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Experimental/ShootButtonHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Scripts;
+using Assets.Scripts.Framework.Experimental;
+
+namespace Assets.Scripts.Game.Experimental
+{
+    internal class ShootButtonHandler : MonoBehaviour, Buttons.IButtonHandler
+    {
+        protected IShootButtonFramework frameworkButtonHandler;
+        protected IBallisticFramework ballistics;
+
+        void Start()
+        {
+            //game object needs MonoBehaviour class, Button needs Buttons namespace
+            //Start() is method in MonoBehaviour
+            Button button = gameObject.GetComponent<Button>();
+            button.onClick.AddListener(Click);
+
+            frameworkButtonHandler = new ShootButtonFramework();
+            ballistics = new BallisticFramework();
+
+        }
+        //updated for each frame
+        void Update()
+        {
+            ballistics.OnUpdate();
+        }
+        //calls for button click
+        public void Click()
+        {
+            if (frameworkButtonHandler != null)
+                frameworkButtonHandler.OnClick();
+            if (ballistics != null)
+                ballistics.Init();
+        }
+
+        public void Pressed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Released()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
