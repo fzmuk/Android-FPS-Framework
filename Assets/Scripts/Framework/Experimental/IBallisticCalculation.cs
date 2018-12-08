c12f9088ce93e463e0b6a590776f0f5fc1f59960
Elvis Popović <elvis_popovic@yahoo.com>
2018-12-08 19:06:42 +0100

    Added ballistic calculations
    Ballistic calculations for fast projectille added. Using Rungle-Kutta 4 to calculate trajectory.

---
+++ Assets/Scripts/Framework/Experimental/IBallisticCalculation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Framework.Experimental
+{
+    interface IBallisticCalculation
+    {
+        void Calculate(ref double vx, ref double vy, ref double x, ref double y, ref double angle, ref double time, double step);
+    }
+}
