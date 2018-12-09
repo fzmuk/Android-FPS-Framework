c3445684c4a0f9aee2bb12c58d8d386c225c4f55
unknown <ppoljak@foi.hr>
2018-12-09 17:34:39 +0100

    Added enemy movement

---
+++ Assets/Scripts/Framework/Enemy/Enemy.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemy
+{
+    public class Enemy
+    {
+        public NavMeshAgent Agent;
+        private float detectionDistance;
+        private int speed;
+        private int acceleration;
+        private int stopingDistance;
+
+        private float range;
+
+        public Enemy(NavMeshAgent agent)
+        {
+            Agent = agent;
+        }
+
+        public int StopingDistance
+        {
+            get
+            {
+                return stopingDistance;
+            }
+            set
+            {
+                Agent.stoppingDistance = value;
+            }
+        }
+
+        public int Acceleration
+        {
+            get
+            {
+                return acceleration;
+            }
+            set
+            {
+                Agent.acceleration = value;
+            }
+        }
+
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                Agent.speed = value;
+            }
+        }
+
+        public float DetectionDistance
+        {
+            get
+            {
+                return detectionDistance;
+            }
+            set
+            {
+                detectionDistance = value;
+            }
+        }
+        //health, armor
+        public float Range;
+
+        public GameObject FindOponentByName(string name)
+        {
+            GameObject oponent = GameObject.Find(name);
+            return oponent;
+        }
+        public GameObject FindOponentByTag(string tag)
+        {
+            GameObject oponent = GameObject.FindGameObjectWithTag(tag);
+            return oponent;
+        }
+
+    }
+}
